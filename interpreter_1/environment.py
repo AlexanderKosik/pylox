ab2dbcf5041ecd11c1c36f5ecd27e4645117de6b
Alexander Kosik <akosik@t-online.de>
2022-08-10 18:41:24 +0200

    Add environment (more or less a dict)

---
+++ interpreter_1/environment.py
@@ -0,0 +1,14 @@
+from loxtoken import LoxToken
+
+class Environment:
+    def __init__(self):
+        self.values = {}
+
+    def define(self, name: LoxToken, value):
+        self.values[name] = value
+
+    def get(name: LoxToken):
+        try:
+            return self.values[name.lexeme]
+        except KeyError:
+            raise Exception("Undefined variable '{name.lexeme}'
