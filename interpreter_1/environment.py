8c54dbc77c2ce2ffdaf1174e5b1d9db19517ae3f
Alexander Kosik <akosik@t-online.de>
2022-08-25 21:43:00 +0200

    Add enclosing environments

--- interpreter_1/environment.py
+++ interpreter_1/environment.py
@@ -1,14 +1,20 @@
 from loxtoken import LoxToken
 
 class Environment:
-    def __init__(self):
+    def __init__(self, enclosing=None):
         self.values = {}
+        self.enclosing = enclosing
 
     def define(self, name: str, value):
         self.values[name] = value
 
     def get(self, name: str):
-        try:
+        # first try the 'local scope'
+        if name in self.values:
             return self.values[name]
-        except KeyError:
-            raise Exception("Undefined variable '{name}'")
+
+        # check surrounding scopes recursivly
+        if self.enclosing is not None:
+            return enclosing.get(name)
+        
+        raise Exception("Undefined variable '{name}'")
